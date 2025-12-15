8a790fbdf9cd6a143b6a4c376e4dbf5a770ad2fe
Rajaputra Arun Kumar <arun.rajaputhra1@gmail.com>
2025-12-16 05:26:12 +0530

    change17

--- app.py
+++ app.py
@@ -296,7 +296,7 @@ def main():
     st.markdown("""
     <div style="text-align: center;">
         <h6 style="margin-bottom: 0; font-size: 1.8rem; color: #00d2ff;"> QUANTUM PREDICTIONS</h6>
-        <p style="margin-top: -5; font-size: 0.8rem; color: #a0d2ff; letter-spacing: 1px;">
+        <p style="margin-top: -20; font-size: 0.8rem; color: #a0d2ff; letter-spacing: 1px;">
             AI-ENHANCED FORECASTING & SIMULATION
     </p>
     </div>
