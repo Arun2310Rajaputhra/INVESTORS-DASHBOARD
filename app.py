6cf539481ba59dcd73ad3a24f7bc4660b716749c
Rajaputra Arun Kumar <arun.rajaputhra1@gmail.com>
2026-01-02 17:21:23 +0530

    01/02/2026 app update

--- app.py
+++ app.py
@@ -643,7 +643,7 @@ def main():
                     # Payment status filter
                     payment_status = st.selectbox(
                         "Payment Status:",
-                        ["All", "Completed", "Pending", "Recovered"]
+                        ["All", "Completed", "Pending", "Recovered", "Adjusted To Self Invest"]
                     )
                 else:
                     st.error("User not found in records.")
